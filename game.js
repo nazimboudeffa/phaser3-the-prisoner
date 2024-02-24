1075c28fa375c5b2c257fa6260b4a70cc21c4305
Nazim Boudeffa <boudeffa@gmail.com>
2024-02-24 14:55:06 +0100

    Added some features

--- game.js
+++ game.js
@@ -10,7 +10,51 @@ class LoadScene extends Phaser.Scene
         this.load.image('six', 'assets/menu/six.png');
         this.load.image('start', 'assets/menu/start.png');
 
-        this.load.image('phone', 'assets/scenes/scene-phone.png');
+        this.load.image('information', 'assets/sprites/information.png');
+        this.load.image('village', 'assets/map/map.png');
+        this.load.image('bicycle', 'assets/sprites/menu-bicycle.png');
+
+        this.load.image('map-pawn', 'assets/map/map-pawn.png');
+        this.load.image('scene-chess', 'assets/scenes/scene-chess.png');
+    
+        this.load.image('map-helico', 'assets/map/map-helicopter.png');
+        this.load.image('scene-helico', 'assets/scenes/scene-helicopter.png');
+    
+        this.load.image('map-freesea', 'assets/map/map-freesea.png');
+        this.load.image('scene-freesea', 'assets/scenes/scene-freesea.png');
+    
+        this.load.image('map-ship', 'assets/map/map-ship.png');
+        this.load.image('scene-ship', 'assets/scenes/scene-ship.png');
+    
+        this.load.image('map-shop', 'assets/map/map-shop.png');
+        this.load.image('scene-shop', 'assets/scenes/scene-shop.png');
+    
+        this.load.image('map-two', 'assets/map/map-two.png');
+        this.load.image('scene-two', 'assets/scenes/scene-dome.png');
+    
+        this.load.image('map-control', 'assets/map/map-control.png');
+        this.load.image('scene-control', 'assets/scenes/scene-town-hall.png');
+    
+        this.load.image('map-labour-exchange', 'assets/map/map-labour-exchange.png');
+        this.load.image('scene-labour-exchange', 'assets/scenes/scene-labour-exchange.png');
+    
+        this.load.image('map-sea', 'assets/map/map-sea.png');
+        this.load.image('scene-sea', 'assets/scenes/scene-sea.png');
+    
+        this.load.image('map-beach', 'assets/map/map-beach.png');
+        this.load.image('scene-beach', 'assets/scenes/scene-beach.png');
+    
+        this.load.image('map-hospital', 'assets/map/map-hospital.png');
+        this.load.image('scene-hospital', 'assets/scenes/scene-hospital.png');
+    
+        this.load.image('map-cafe', 'assets/map/map-cafe.png');
+        this.load.image('scene-cafe', 'assets/scenes/scene-cafe.png');
+    
+        this.load.image('map-private', 'assets/map/map-six-private.png');
+        this.load.image('scene-private', 'assets/scenes/scene-six-private.png');
+    
+        this.load.image('map-phone', 'assets/map/map-phone.png');
+        this.load.image('scene-phone', 'assets/scenes/scene-phone.png');
 
         let loadingBar = this.add.graphics({
             fillStyle: {
@@ -73,7 +117,54 @@ class PlayScene extends Phaser.Scene
 
     create ()
     {
-        this.add.image(400, 300, 'phone');
+        this.add.image(0, 0, 'scene-phone').setOrigin(0, 0);
+
+        const information = this.add.sprite(429, 137, 'information').setOrigin(0, 0).setInteractive({ cursor: 'pointer' });
+
+        information.on('pointerover', function (event)
+        {
+            this.setTint(0xff0000);
+        });
+
+        information.on('pointerout', function (event)
+        {
+            this.clearTint();
+        });
+
+        information.on('pointerdown', () => {
+            this.scene.start('map');
+        });
+    }
+}
+
+class MapScene extends Phaser.Scene
+{
+    constructor()
+    {
+        super({ key: 'map', active: false });
+    }
+
+    create ()
+    {
+        this.add.image(400, 300, 'village');
+
+        const phone = this.add.sprite(511, 272, 'map-phone').setOrigin(0, 0).setInteractive({ cursor: 'pointer' });
+        phone.setTint(0x00ff00);
+
+        var cam = this.cameras.main;
+        //cam.setZoom(2);
+
+        this.input.on("pointermove", function (p) {
+            if (!p.isDown) return;
+        
+            cam.scrollX -= (p.x - p.prevPosition.x) / cam.zoom;
+            cam.scrollY -= (p.y - p.prevPosition.y) / cam.zoom;
+        });
+
+        phone.on('pointerdown', () =>
+        {
+            this.scene.start('play');
+        });
     }
 }
 
@@ -84,7 +175,7 @@ const config = {
     width: 800,
     height: 600,
     scene: [
-        LoadScene, MenuScene, PlayScene
+        LoadScene, MenuScene, PlayScene, MapScene
     ]
 };
 
